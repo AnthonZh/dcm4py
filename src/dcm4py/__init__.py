448108ec6034d3458b70ad949aa0c64e4ef23dde
AnthonZh <anthonyzh3349@gmail.com>
2024-11-08 14:53:11 -0500

    Add pyproject.toml, modify init to include the information flit requires

--- src/dcm4py/__init__.py
+++ src/dcm4py/__init__.py
@@ -1,1 +1,5 @@
 from dcm4py import dcm2npy, npy2dcm, normalize_array
+
+"""A package which allows simple conversion of DICOM images to NumPy arrays and NumPy arrays to DICOM images"""
+
+__version__ = "0.1"
